21dda2da918365d14e624627e49088eab1991763
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-05 04:21:12 +0300

    reformating main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 /*
  * parsing grammar:
- * Expression: 
+ * Expression:
  *   Term + Term
  *   Term - Term
  *   Let var_name = Expression
@@ -24,6 +24,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+
 #include "token.h"
 #include "utils.h"
 
@@ -34,145 +35,114 @@ double power();
 
 Token_stream ts;
 
-double expression()
-{
+double expression() {
     double left = term();
-    Token  next = ts.get();
-    while (true)
-    {
-        switch (next.kind)
-        {
-            case '+':
-                left += term();
-                next = ts.get();
-                break;
-            case '-':
-                left -= term();
-                next = ts.get();
-                break;
-            default:
-                ts.putback(next);
-                return left;
-        }
-    }
-}
-
-double term()
-{
-    double left = power();
-    Token  next = ts.get();
-    while (true)
-    {
-        switch (next.kind)
-        {
-            case '*':
-                left *= power();
-                next = ts.get();
-                break;
-            case '/':
-            {
-                double d = power();
-                if (d == 0)
-                    throw std::runtime_error(color("error: devision by zero", 'r'));
-                left /= d;
-                next = ts.get();
-                break;
-            }
-            default:
-                ts.putback(next);
-                return left;
-        }
-    }
-}
-
-double power()
-{
-    double left = primary();
-    Token  next = ts.get();
-    while (true)
-    {
-        if (next.kind == '^')
-        {
-            return std::pow(left, primary());
+    Token next = ts.get();
+    while (true) {
+        switch (next.kind) {
+        case '+':
+            left += term();
             next = ts.get();
-        }
-        else
-        {
+            break;
+        case '-':
+            left -= term();
+            next = ts.get();
+            break;
+        default:
             ts.putback(next);
             return left;
         }
     }
 }
 
-double primary()
-{
+double term() {
+    double left = power();
     Token next = ts.get();
-    switch (next.kind)
-    {
-        case '(':
-        {
-            double d = expression();
-            next     = ts.get();
-            if (next.kind != ')')
-                throw std::runtime_error(color("error: ')' expected", 'r'));
-            Token is_fac = ts.get();
-            if (is_fac.kind == '!')
-            {
-                return factorial(d);
-            }
-            else
-            {
-                ts.putback(is_fac);
-            }
-            return d;
+    while (true) {
+        switch (next.kind) {
+        case '*':
+            left *= power();
+            next = ts.get();
+            break;
+        case '/': {
+            double d = power();
+            if (d == 0)
+                throw std::runtime_error(color("error: devision by zero", 'r'));
+            left /= d;
+            next = ts.get();
             break;
         }
-        case '8':
-        {
-            Token is_fac = ts.get();
-            if (is_fac.kind == '!')
-            {
-                return factorial(next.value);
-            }
-            else
-            {
-                ts.putback(is_fac);
-            }
-            return next.value;
-            break;
-        }
-        case 'q':
-            std::cout << "Exiting...\n";
-            exit(0);
         default:
-            throw std::runtime_error(color("error: primary expected", 'r'));
-            break;
+            ts.putback(next);
+            return left;
+        }
     }
 }
 
-int main()
-{
-    while (std::cout << "> ")
-    {
-        try
-        {
+double power() {
+    double left = primary();
+    Token next = ts.get();
+    while (true) {
+        if (next.kind == '^') {
+            return std::pow(left, primary());
+            next = ts.get();
+        } else {
+            ts.putback(next);
+            return left;
+        }
+    }
+}
+
+double primary() {
+    Token next = ts.get();
+    switch (next.kind) {
+    case '(': {
+        double d = expression();
+        next = ts.get();
+        if (next.kind != ')')
+            throw std::runtime_error(color("error: ')' expected", 'r'));
+        Token is_fac = ts.get();
+        if (is_fac.kind == '!') {
+            return factorial(d);
+        } else {
+            ts.putback(is_fac);
+        }
+        return d;
+        break;
+    }
+    case '8': {
+        Token is_fac = ts.get();
+        if (is_fac.kind == '!') {
+            return factorial(next.value);
+        } else {
+            ts.putback(is_fac);
+        }
+        return next.value;
+        break;
+    }
+    case 'q':
+        std::cout << "Exiting...\n";
+        exit(0);
+    default:
+        throw std::runtime_error(color("error: primary expected", 'r'));
+        break;
+    }
+}
+
+int main() {
+    while (std::cout << "> ") {
+        try {
             double val = expression();
-            Token  t   = ts.get();
-            if (t.kind == ';')
-            {
-                std::cout << "= " << color(std::format("{}", val), 'g') << std::endl;
-            }
-            else
-            {
+            Token t = ts.get();
+            if (t.kind == ';') {
+                std::cout << "= " << color(std::format("{}", val), 'g')
+                          << std::endl;
+            } else {
                 std::cout << "';' expected" << std::endl;
             }
-        }
-        catch (std::exception &e)
-        {
+        } catch (std::exception &e) {
             std::cerr << e.what() << std::endl;
-        }
-        catch (...)
-        {
+        } catch (...) {
             std::cerr << "unknown error happend" << std::endl;
         }
     }
