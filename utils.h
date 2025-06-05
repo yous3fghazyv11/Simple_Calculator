bf62ed270044f301279920ff5cbbd62fa543b3f5
yous3fghazyv11 <yous3fghazyv11@gmail.com>
2025-06-05 04:23:13 +0300

    reformating utils.h

--- utils.h
+++ utils.h
@@ -4,28 +4,26 @@
 #include <stdexcept>
 #include <string>
 
-inline std::string color(std::string text, char color)
-{
+inline std::string color(std::string text, char color) {
     std::string green = "\033[32m";
-    std::string red   = "\033[31m";
+    std::string red = "\033[31m";
     std::string white = "\033[0m";
-    switch (color)
-    {
-        case 'r':
-            return red + text + white;
-            break;
-        case 'g':
-            return green + text + white;
-            break;
-        default:
-            return "";
+    switch (color) {
+    case 'r':
+        return red + text + white;
+        break;
+    case 'g':
+        return green + text + white;
+        break;
+    default:
+        return "";
     }
 }
 
-inline double factorial(double num)
-{
+inline double factorial(double num) {
     if (num < 0 || std::floor(num) != num)
-        throw std::runtime_error(color("Factorial of negative/non-integer", 'r'));
+        throw std::runtime_error(
+            color("Factorial of negative/non-integer", 'r'));
     if (num == 1 || num == 0)
         return 1;
     return num * (factorial(num - 1));
